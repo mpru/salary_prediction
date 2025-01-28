4cb5e38be8888bd2ed27421dbbb1e535bba4f527
marcos-nb <marcosprunello@gmail.com>
2025-01-27 23:26:18 -0300

    added exploratory data analysis, script 01

--- code/modulos.py
+++ code/modulos.py
@@ -41,3 +41,35 @@ def explore_missing(datos):
     print(missing_summary)
 
 import pandas as pd
+
+
+# Crear tablas con frecuencia absoluta y porcentaje
+def create_frequency_table(column, column_name):
+    """
+    Creates a frequency table with both absolute frequency (N) and percentage for a given column in a DataFrame.
+    The function also adds a row with total counts and percentage values.
+
+
+    Args:
+    column (pd.Series): The input column (Series) from a DataFrame for which the frequency table will be created.
+    column_name (str): The name of the column, which will be used as the header in the resulting frequency table.
+
+    Returns:
+    pd.DataFrame: A DataFrame containing the frequency table with the following columns:
+        - column_name: The unique values/categories in the input column.
+        - N: The absolute frequency of each value/category.
+        - Percentage: The percentage of each value/category based on the total count.
+    """
+
+    freq_table = column.value_counts(dropna=False).reset_index()
+    freq_table.columns = [column_name, "N"]
+    freq_table["Percentage"] = (freq_table["N"] / column.size * 100).round(2)
+    
+    # Agregar fila de totales
+    total_row = pd.DataFrame({
+        column_name: ["Total"],
+        "N": [freq_table["N"].sum()],
+        "Percentage": [freq_table["Percentage"].sum()]
+    })
+    freq_table = pd.concat([freq_table, total_row], ignore_index=True)
+    return freq_table
